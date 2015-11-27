fe498ad09a4b51c4e15ad3b1c120c75ab5455580
Sebastian Weigelt <weigelt@kit.edu>
2015-11-27 13:47:04 +0100

    add filestystem handling

--- src/main/java/edu/kit/ipd/parse/parsebios/Facade.java
+++ src/main/java/edu/kit/ipd/parse/parsebios/Facade.java
@@ -4,9 +4,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,10 +35,12 @@ public class Facade {
 			if (words != null && pos != null && words.length == pos.length) {
 				URL resourceUrl = getClass().getResource("/model");
 				logger.info("Loaded URL {}", resourceUrl.toString());
+				Map<String, String> env = new HashMap<>();
+				env.put("create", "true");
+				FileSystem zipfs = FileSystems.newFileSystem(resourceUrl.toURI(), env);
 				Path resourcePath = Paths.get(resourceUrl.toURI());
 				logger.info("Loaded resource path {}", resourcePath.toString());
-				//				Chunker chk = new Chunker(resourcePath.toString(), "conll.paum.cs.model", ChunkerConstants.PAUM, true, false);
-				Chunker chk = new Chunker(resourceUrl.toString(), "conll.paum.cs.model", ChunkerConstants.PAUM, true, false);
+				Chunker chk = new Chunker(resourcePath.toString(), "conll.paum.cs.model", ChunkerConstants.PAUM, true, false);
 				logger.info("Chunker is {}", chk.toString());
 				ArrayList<PosToken> list = new ArrayList<PosToken>();
 				for (int i = 0; i < words.length; i++) {
