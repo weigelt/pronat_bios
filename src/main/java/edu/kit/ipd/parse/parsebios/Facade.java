c90108f0ab64febb2e121cd695a1b6ef1562ee3e
Sebastian Weigelt <weigelt@kit.edu>
2015-11-27 16:47:53 +0100

    implemented resource file copy mechanisms

--- src/main/java/edu/kit/ipd/parse/parsebios/Facade.java
+++ src/main/java/edu/kit/ipd/parse/parsebios/Facade.java
@@ -3,14 +3,15 @@ package edu.kit.ipd.parse.parsebios;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +23,7 @@ import bios.common.PosToken;
 public class Facade {
 
 	private static final Logger logger = LoggerFactory.getLogger(Facade.class);
+	private static final String resourceModelPath = "/model/conll.paum.cs.model";
 
 	public void parseFile(File file) throws Exception {
 		Chunker chk = new Chunker("target/classes/model", "conll.paum.cs.model", ChunkerConstants.PAUM, true, false);
@@ -31,18 +33,20 @@ public class Facade {
 	}
 
 	public String[] parse(String[] words, String[] pos) {
+
+		List<String> resList = new ArrayList<String>() {
+			{
+				add(resourceModelPath);
+				add(resourceModelPath + ".features");
+				add(resourceModelPath + ".labels");
+			}
+		};
+
 		try {
 			if (words != null && pos != null && words.length == pos.length) {
-				URL resourceUrl = getClass().getResource("/model");
-				logger.info("Loaded URL {}", resourceUrl.toString());
-				Map<String, String> env = new HashMap<>();
-				env.put("create", "true");
-				FileSystem zipfs = FileSystems.newFileSystem(resourceUrl.toURI(), env);
-				Path resourcePath = Paths.get(resourceUrl.toURI());
-				logger.info("Loaded resource path {}", resourcePath.toString());
-				Chunker chk = new Chunker(
-						"C:/Users/Sebastian_Weigelt/.m2/repository/edu/kit/ipd/parse/parseBios/1.0-SNAPSHOT/parseBios-1.0-SNAPSHOT.jar!/model",
-						"conll.paum.cs.model", ChunkerConstants.PAUM, true, false);
+
+				Path tempDirPath = copyResourcesToTempDir(resList);
+				Chunker chk = new Chunker(tempDirPath.toString(), "conll.paum.cs.model", ChunkerConstants.PAUM, true, false);
 				logger.info("Chunker is {}", chk.toString());
 				ArrayList<PosToken> list = new ArrayList<PosToken>();
 				for (int i = 0; i < words.length; i++) {
@@ -59,4 +63,18 @@ public class Facade {
 		return null;
 	}
 
+	private Path copyResourcesToTempDir(List<String> resList) throws IOException, URISyntaxException {
+		Path tempDirPath = Files.createTempDirectory(null);
+		URL resourceUrl;
+		InputStream resourceIS;
+		Path resourcePath;
+
+		for (String res : resList) {
+			resourceUrl = getClass().getResource(res);
+			resourceIS = getClass().getResourceAsStream(res);
+			resourcePath = Paths.get(getClass().getResource(res).toURI());
+			Files.copy(resourceIS, tempDirPath.resolve(resourcePath.getFileName()));
+		}
+		return tempDirPath;
+	}
 }
