990f2e626cc1f249878cd1d2989692b7b3af5d27
Sebastian Weigelt <weigelt@kit.edu>
2016-06-08 19:32:04 +0200

    add test

--- src/test/java/edu/kit/ipd/parse/parsebios/SimpleFacadeTest.java
+++ src/test/java/edu/kit/ipd/parse/parsebios/SimpleFacadeTest.java
@@ -3,8 +3,6 @@ package edu.kit.ipd.parse.parsebios;
 import org.junit.Assert;
 import org.junit.Test;
 
-import edu.kit.ipd.parse.parsebios.Facade;
-
 public class SimpleFacadeTest {
 
 	@Test
@@ -12,6 +10,17 @@ public class SimpleFacadeTest {
 		String[] words = { "My", "Name", "is", "Markus" };
 		String[] pos = { "PRP$", "NN", "VBZ", "NNP" };
 		String[] exp = { "B-NP", "I-NP", "B-VP", "B-NP" };
+		Facade a = new Facade();
+		String[] parse = a.parse(words, pos);
+		Assert.assertArrayEquals(exp, parse);
+
+	}
+
+	@Test
+	public void testADVP() {
+		String[] words = { "If", "the", "black", "dog", "barks", "loudly", "go" };
+		String[] pos = { "IN", "DT", "JJ", "NN", "VBZ", "RB", "VB" };
+		String[] exp = { "B-SBAR", "B-NP", "I-NP", "I-NP", "B-VP", "B-ADVP", "B-VP" };
 		Facade a = new Facade();
 		String[] parse = a.parse(words, pos);
 		Assert.assertArrayEquals(exp, parse);
